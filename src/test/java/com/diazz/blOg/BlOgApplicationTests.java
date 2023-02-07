3dc438cf8185bd7ee9a3b48e23aa7a214e156c80
zarina <nurzanovazarina1@gmail.com>
2023-02-07 19:44:32 +0600

    Creating project

---
+++ src/test/java/com/diazz/blOg/BlOgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diazz.blOg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlOgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
