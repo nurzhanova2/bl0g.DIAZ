0d42483041ff4d516a4b0c54fcd7e41ec551502d
zarina <nurzanovazarina1@gmail.com>
2023-02-07 20:06:00 +0600

    details controller

--- src/main/java/com/diazz/blOg/controllers/DetailsController.java
+++ src/main/java/com/diazz/blOg/controllers/DetailsController.java
@@ -1,4 +1,21 @@
 package com.diazz.blOg.controllers;
+import com.diazz.blOg.models.Post;
+import com.diazz.blOg.repo.PostRepository;
+import com.diazz.blOg.service.impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
 public class DetailsController {
+    @Autowired
+    private PostRepository PostRepository;
+
+
 }
