95c76f1202e21e0cdffdc37a2a2192e0276690f1
nurzhanova2 <nurzanovazarina1@gmail.com>
2023-02-08 22:33:16 +0600

    This commit updates the Post class by adding a constructor with 4 parameters and getter/setter methods for all the class fields. The purpose of this change is to initialize a Post object with values for title, anons, full text and views. The author of the commit is zarina and it was made on [08.02.2023]

--- src/main/java/com/diazz/blOg/models/Post.java
+++ src/main/java/com/diazz/blOg/models/Post.java
@@ -23,4 +23,61 @@ public class Post {
 
     @Column(name = "views")
     private int views;
+    public Post( String title, String anons, String full_text, int views) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+        this.views = views;
+    }
+
+    public Post() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnons() {
+        return anons;
+    }
+
+    public void setAnons(String anons) {
+        this.anons = anons;
+    }
+
+    public String getFull_text() {
+        return full_text;
+    }
+
+    public void setFull_text(String full_text) {
+        this.full_text = full_text;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 }
