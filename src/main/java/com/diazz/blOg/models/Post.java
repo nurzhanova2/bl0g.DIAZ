20639b53abf0fc03f31d8e2e0ceb4e34f25e3b8a
zarina <nurzanovazarina1@gmail.com>
2023-02-08 11:44:06 +0600

    class Post

--- src/main/java/com/diazz/blOg/models/Post.java
+++ src/main/java/com/diazz/blOg/models/Post.java
@@ -1,4 +1,14 @@
 package com.diazz.blOg.models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+package com.diazz.blOg.models;
+
 public class Post {
+
 }
+
+
