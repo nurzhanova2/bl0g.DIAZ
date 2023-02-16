10a00e7da75cd3ec2cf285ef128f2b9abbebb13d
DiasGuitar <brainwin0@gmail.com>
2023-02-16 14:20:53 +0600

    some changes

--- src/main/java/com/diazz/blOg/models/Post.java
+++ src/main/java/com/diazz/blOg/models/Post.java
@@ -1,20 +1,6 @@
 package com.diazz.blOg.models;
-import jakarta.persistence.*;
-import org.apache.el.parser.ParseException;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.*;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 
-import javax.swing.text.Document;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.ArrayList;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "post")
