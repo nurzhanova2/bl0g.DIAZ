5ceeb28753ee0b487c5596e109f1690cd3595ce6
nurzhanova2 <nurzanovazarina1@gmail.com>
2023-02-19 17:32:18 +0600

    abstract class postService

--- src/main/java/com/diazz/blOg/service/postService.java
+++ src/main/java/com/diazz/blOg/service/postService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class postService {
+public abstract class postService {
 
     @Autowired
     private PostRepository postRepository;
@@ -18,7 +18,5 @@ public class postService {
         this.postRepository = postRepository;
     }
 
-    public List<Post> getAllPosts(){
-        return postRepository.findAll();
-    }
+    public abstract List<Post> getAllPosts();
 }
